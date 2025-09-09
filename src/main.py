7819325af63b9b97217fb1a78ef1a407f57f5b2a
Ellu <alikoskieveliina@gmail.com>
2025-09-09 11:51:35 +0300

    initializing project with poetry

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from tkinter import Tk
+from ui import UI
+from app import Connect4
+
+def main():
+    window = Tk()
+    window.title("Connect4")
+
+    game = Connect4()
+    ui = UI(window, game)
+    ui.start()
+
+    window.mainloop()
+
+if __name__=="__main__":
+    main()
