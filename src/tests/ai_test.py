2cfdb012ad464476e16cbf2d9ebd81d2196d8f96
Ellu <alikoskieveliina@gmail.com>
2025-09-19 17:06:38 +0300

    fix tests and tests for winning_move() and heuristic_value()

--- src/tests/ai_test.py
+++ src/tests/ai_test.py
@@ -5,27 +5,69 @@ class TestAI(unittest.TestCase):
     def setUp(self):
         self.ai = AI()
 
-    def test_get_moves_with_empty_board(self):
-        gameboard = [[0 for col in range(self.ai.cols)]
-                      for row in range(self.ai.rows)]
-    
-        possible_moves = self.ai.get_moves(gameboard)
+        self.empty_board = [[0 for col in range(self.ai.cols)]
+                       for row in range(self.ai.rows)]
 
-        correct_moves = [0, 1, 2, 3, 4, 5, 6]
+    def test_winning_move_with_no_win(self):
+        gameboard = self.empty_board
+        gameboard[5][2] = 1
+        result = self.ai.winning_move(gameboard, 5, 2)
+
+        self.assertEqual(result, False)
+
+    def test_winning_move_horizontal_win(self):
+        gameboard = self.empty_board
+        gameboard[5][2] = 1
+        gameboard[4][2] = 5
+        gameboard[5][3] = 1
+        gameboard[5][1] = 5
+        gameboard[5][4] = 1
+        gameboard[4][3] = 5
+        gameboard[5][5] = 1
+
+        result = self.ai.winning_move(gameboard, 5, 6)
+
+        self.assertEqual(result, True)
+
+    def test_winning_move_vertical_win(self):
+        gameboard = self.empty_board
+        gameboard[5][0] = 1
+        gameboard[5][3] = 5
+        gameboard[4][0] = 1
+        gameboard[5][2] = 5
+        gameboard[3][0] = 1
+        gameboard[5][3] = 5
+        gameboard[2][0] = 1
+
+        result = self.ai.winning_move(gameboard, 2, 0)
+
+        self.assertEqual(result, True)
+
+    def test_heuristic_value_with_one_move(self):
+        gameboard = self.empty_board
+        gameboard[5][3] = 1
+        
+        score = self.ai.heuristic_value(gameboard)
+
+        self.assertEqual(score, -70)
+
+    def test_get_moves_with_empty_board(self):
+        possible_moves = self.ai.get_moves(self.empty_board)
+
+        correct_moves = [3, 2, 4, 1, 5, 0, 6]
 
         self.assertEqual(possible_moves, correct_moves)
 
     def test_get_moves_with_partially_filled_board(self):
-        gameboard = [[0 for col in range(self.ai.cols)]
-                      for row in range(self.ai.rows)]
+        gameboard = self.empty_board
         gameboard[5][3] = 1
-        gameboard[5][2] = 2
+        gameboard[5][2] = 5
         gameboard[4][3] = 1
-        gameboard[3][3] = 2
+        gameboard[3][3] = 5
     
         possible_moves = self.ai.get_moves(gameboard)
 
-        correct_moves = [0, 1, 2, 3, 4, 5, 6]
+        correct_moves = [3, 2, 4, 1, 5, 0, 6]
 
         self.assertEqual(possible_moves, correct_moves)
 
@@ -40,19 +82,18 @@ class TestAI(unittest.TestCase):
         self.assertEqual(possible_moves, correct_moves)
 
     def test_simulate_move_with_partially_filled_board(self):
-        gameboard = [[0 for col in range(self.ai.cols)]
-                     for row in range(self.ai.rows)]
+        gameboard = self.empty_board
         gameboard[5][3] = 1
-        gameboard[5][2] = 2
+        gameboard[5][2] = 5
         gameboard[4][3] = 1
-        gameboard[3][3] = 2
+        gameboard[3][3] = 5
 
         new_board = self.ai.simulate_move(gameboard, 2, False)
 
         correct_board = gameboard
         correct_board[4][2] = 1
 
-        self.assertEqual(new_board, correct_board)
+        self.assertEqual(new_board, (correct_board, (4, 2)))
 
 
 ## TO DO johonkin valmiiksi empty board, partially filled board 
