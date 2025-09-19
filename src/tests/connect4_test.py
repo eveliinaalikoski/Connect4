3784aea419b9f52d88bb07b444ce7fd50fe0f4f1
Ellu <alikoskieveliina@gmail.com>
2025-09-19 17:05:51 +0300

    fix tests and tests for full_board()

--- src/tests/connect4_test.py
+++ src/tests/connect4_test.py
@@ -3,7 +3,7 @@ from app import Connect4
 
 class TestConnect4(unittest.TestCase):
     def setUp(self):
-        self.game = Connect4()
+        self.game = Connect4(root=None)
 
     def test_make_move_successful_when_wanted_column_is_not_full(self):
         result = self.game.make_move(3, 1) # pelaaja tekee siirron keskimmäiseen sarakkeeseen
@@ -12,26 +12,41 @@ class TestConnect4(unittest.TestCase):
                       for row in range(self.game.rows)]
         wanted_board[5][3] = 1
 
-        self.assertEqual(result, True)
+        self.assertEqual(result, (True, (5,3)))
         self.assertEqual(self.game.board, wanted_board)
 
     def test_make_move_unsuccessful_when_wanted_column_is_full(self):
         self.game.make_move(3, 1)
-        self.game.make_move(3, 2)
+        self.game.make_move(3, 5)
         self.game.make_move(3, 1)
-        self.game.make_move(3, 2)
+        self.game.make_move(3, 5)
         self.game.make_move(3, 1)
-        self.game.make_move(3, 2) # keskikolumni on nyt täynnä
+        self.game.make_move(3, 5) # keskikolumni on nyt täynnä
         result = self.game.make_move(3, 1)
 
         wanted_board = [[0 for col in range(self.game.cols)]
                       for row in range(self.game.rows)]
         wanted_board[5][3] = 1
-        wanted_board[4][3] = 2
+        wanted_board[4][3] = 5
         wanted_board[3][3] = 1
-        wanted_board[2][3] = 2
+        wanted_board[2][3] = 5
         wanted_board[1][3] = 1
-        wanted_board[0][3] = 2
+        wanted_board[0][3] = 5
+
+        self.assertEqual(result, (False, None))
+        self.assertEqual(self.game.board, wanted_board)
+    
+    def test_full_board_returns_true_if_board_is_full(self):
+        self.game.board = [[1 for col in range(self.game.cols)]
+                           for row in range(self.game.rows)]
+        
+        result = self.game.full_board()
+
+        self.assertEqual(result, True)
+
+    def test_full_board_returns_false_if_board_is_not_full(self):
+        self.game.board[5][3] = 1
+
+        result = self.game.full_board()
 
         self.assertEqual(result, False)
-        self.assertEqual(self.game.board, wanted_board)
